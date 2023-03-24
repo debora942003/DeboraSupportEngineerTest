98273267126fe333344af83e4c655644a369dda6
root <root@Debora>
2023-03-24 15:58:53 +0100

    Correzione dei commenti

--- esercizio1/script.sh
+++ esercizio1/script.sh
@@ -9,9 +9,9 @@ echo "Stringa 1: $stringa1";
 echo "Stringa 2: $stringa2";
 echo "Nome directory: $directory";
 
-#Processiamo i file all'interno della direcfile
+#Processiamo i file all'interno della directory
 #File è il nome della variabile che conterrà il nome del file nella directory per ciascun giro del ciclo for
-#Quindi se dentro la directory avremo 3 file, al primo giro file avrà il valore del nome del primo filo
+#Quindi se dentro la directory avremo 3 file, al primo giro file avrà il valore del nome del primo file
 #al secondo giro file avrà il valore del nome del secondo file e così via.
 for file in "$directory"/*
 do 
